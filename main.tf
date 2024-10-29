4f0ecd5296be9f1369274101975b74ebbd6ddb1b
MufaroDKaseke <mufarodkaseke@gmail.com>
2024-10-29 09:27:53 +0200

    Added resource group and vn

--- main.tf
+++ main.tf
@@ -8,5 +8,22 @@ terraform {
 }
 
 provider "azurerm" {
-  feature {}
+  features {
+  }
+  subscription_id = var.sub_id
+}
+
+resource "azurerm_resource_group" "learn_resource_group" {
+  name     = "learn_resource_group"
+  location = "South Africa North"
+  tags = {
+    environment = "learn"
+  }
+}
+
+resource "azurerm_virtual_network" "learn_vn" {
+  name                = "learn_vn"
+  resource_group_name = azurerm_resource_group.learn_resource_group.name
+  location            = azurerm_resource_group.learn_resource_group.location
+  address_space       = ["10.0.0.0/16"]
 }
